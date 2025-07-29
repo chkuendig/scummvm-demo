bf49ccbfe07451f800696e894839f9e95309a5ea
Christian Kündig <christian@kuendig.info>
2025-07-29 22:12:42 +0200

    Update to new ScummVM branch and add scripts removed from main repo

---
+++ scripts/autodetect-games.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+const static = require('node-static');
+
+var file = new static.Server("./", { headers: { 'Cross-Origin-Opener-Policy': 'same-origin','Cross-Origin-Embedder-Policy':'require-corp','Cross-Origin-Resource-Policy':'same-site' } });
+const server = http.createServer(function (req, res) {
+    req.addListener('end', function () {
+        file.serve(req, res);
+    }).resume();
+}).listen({ host: 'localhost', port: 0 },async () => {
+    const browser = await puppeteer.launch({ 
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] // Added for better compatibility
+    });
+    const page = await browser.newPage();
+
+    await page.goto('http://localhost:' + server.address().port + '/scummvm.html#--add --path=/data/games --recursive');
+
+    await page.screenshot({ path: 'example.png' });
+    const regex = /Added ([0-9]+) games/;
+    page.on('console', async msg => {
+        const text = msg.text()
+        console.log(text)
+        const match = text.match(regex);
+        if (match != null && match.length > 0) {
+            console.log("Detection finished, exporting ini file for " + match[1] + " detected games.")
+            const ini_file = await page.evaluate(() => {return FS.readFile("/home/web_user/scummvm.ini", { encoding: 'utf8' })});
+            const ini_lines = ini_file.split('\n');
+            // GRIM games check data consistency by reading all files. That's an expensive operation over
+            // the network. Since we anyway should have known good data at build time, this script disables
+            // that check.
+            for (var i = 0; i < ini_lines.length; i++) {
+                if (ini_lines[i] == "engineid=grim") {
+                    ini_lines[i] = "check_gamedata=false\n" + ini_lines[i]
+                }
+            }
+            fs.writeFileSync("scummvm.ini", ini_lines.join('\n'));
+            await browser.close(); // Added await for proper cleanup
+            server.close();
+
+            console.log('Done');
+        }
+    });
+});
---
+++ scripts/download-games.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# ScummVM is the legal property of its developers, whose names
+# are too numerous to list here. Please refer to the COPYRIGHT
+# file distributed with this source distribution.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+set -e # exit when any command fails
+root_dir="$(pwd)"
+scummvm_dir="$(pwd)/scummvm/"
+
+#################################
+# Create Games & Testbed Data
+#################################
+echo "Creating Games + Testbed Data"
+_bundle_games="${1//,/ }"
+cd "${scummvm_dir}"
+mkdir -p "${scummvm_dir}/build-emscripten/data/games/"
+
+echo ^\(${_bundle_games// /|}\)$
+if [[ "playground3d" =~ $(echo ^\(${_bundle_games// /|}\)$) ]]; then
+    rm -rf "${scummvm_dir}/build-emscripten/data/games/playground3d"
+    cd "${scummvm_dir}/dists/engine-data"
+    ./create-playground3d-data.sh
+    mv playground3d "${scummvm_dir}/build-emscripten/data/games/playground3d"
+fi
+
+if [[ "testbed" =~ $(echo ^\(${_bundle_games// /|}\)$) ]]; then
+    rm -rf "${scummvm_dir}/build-emscripten/data/games/testbed"
+    cd "${scummvm_dir}/dists/engine-data"
+    ./create-testbed-data.sh
+    mv testbed "${scummvm_dir}/build-emscripten/data/games/testbed"
+fi
+
+echo "Fetching games: $_bundle_games"
+
+    cd "${root_dir}"
+    mkdir -p games/
+    cd games/
+    files=$(node --unhandled-rejections=strict --trace-warnings "${root_dir}/scripts/download-games.js" ${_bundle_games})
+    for dir in "${scummvm_dir}/build-emscripten/games/"*/; do # cleanup games folder
+      if [ "$(basename ${dir%*/})" != "testbed" ] && [ "$(basename ${dir%*/})" != "playground3d"  ]; then
+        rm -rf "$dir"
+      fi
+    done
+    for f in $files; do # unpack into games folder
+      echo "Unzipping $f ..."
+      unzip -q -n "$f" -d "${scummvm_dir}/build-emscripten/data/games/${f%.zip}"
+      # some zip files have weird permissions, this fixes that:
+      find "${scummvm_dir}/build-emscripten/data/games/${f%.zip}" -type d -exec chmod 0755 {} \;
+      find "${scummvm_dir}/build-emscripten/data/games/${f%.zip}" -type f -exec chmod 0644 {} \;
+    done
